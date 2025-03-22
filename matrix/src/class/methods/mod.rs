5f4fd17d13d08aecf8754639405b06480d06da41
di3go0-0 <d.rincon@utp.edu.co>
2025-03-21 23:21:00 -0500

    adjugate

--- matrix/src/class/methods/mod.rs
+++ matrix/src/class/methods/mod.rs
@@ -1,2 +1,4 @@
+pub mod adjugate;
 pub mod determinant;
+pub mod inverse;
 pub mod transpose;
--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -5,18 +5,22 @@ fn main() {
     let mut matrix = Matrix::new(3, 3);
     println!("Matrix:\n{}", matrix);
 
-    matrix.set(0, 0, 4.0).unwrap();
-    matrix.set(0, 1, 1.0).unwrap();
-    matrix.set(0, 2, 3.0).unwrap();
+    matrix.set(0, 0, 2.0).unwrap();
+    matrix.set(0, 1, -2.0).unwrap();
+    matrix.set(0, 2, 2.0).unwrap();
     matrix.set(1, 0, 2.0).unwrap();
     matrix.set(1, 1, 1.0).unwrap();
-    matrix.set(1, 2, 4.0).unwrap();
-    matrix.set(2, 0, 0.0).unwrap();
-    matrix.set(2, 1, 1.0).unwrap();
+    matrix.set(1, 2, 0.0).unwrap();
+    matrix.set(2, 0, 3.0).unwrap();
+    matrix.set(2, 1, -2.0).unwrap();
     matrix.set(2, 2, 2.0).unwrap();
 
     println!("Matrix:\n{}", matrix);
 
     let det = matrix.determinant();
     println!("Det: {}", det);
+    let trans = matrix.transpose();
+    println!("trans: \n{}", trans);
+    let adju = trans.adjugte();
+    println!("adju: \n{}", adju);
 }
