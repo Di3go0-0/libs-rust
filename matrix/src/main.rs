6dc4eff3ec3f28fbe0146065f9503611b3bda27a
di3go0-0 <d.rincon@utp.edu.co>
2025-03-22 00:07:10 -0500

    main

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -2,32 +2,15 @@ use class::class::Matrix;
 
 mod class;
 fn main() {
-    let mut matrix = Matrix::new(3, 3);
+    let mut matrix = Matrix::new(2, 3);
     println!("Matrix:\n{}", matrix);
-    let mut matrix1 = Matrix::new(3, 1);
 
-    let x1 = vec![2.0, -2.0, 2.0];
-    let x2 = vec![2.0, 1.0, 0.0];
-    let x3 = vec![3.0, -2.0, 2.0];
+    let x1 = vec![1.0, 2.0, 3.0];
+    let x2 = vec![4.0, 5.0, 6.0];
 
     matrix.set_row(0, x1);
     matrix.set_row(1, x2);
-    matrix.set_row(2, x3);
 
-    matrix1.set(0, 0, 1.0);
-    matrix1.set(1, 0, 1.0);
-    matrix1.set(2, 0, 1.0);
-
-    println!("Matrix:\n{}", matrix);
-    println!("Matrix1:\n{}", matrix1);
-
-    let multy = matrix.multiplication(matrix1);
-    println!("Multi:\n{}", multy);
-
-    // let det = matrix.determinant();
-    // println!("Det: {}", det);
-    // let trans = matrix.transpose();
-    // println!("trans: \n{}", trans);
-    // let adju = trans.adjugte();
-    // println!("adju: \n{}", adju);
+    let pseudo = matrix.inverse();
+    println!("inverse: \n{}", pseudo);
 }
