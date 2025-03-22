991f4ed51bb600ec2087e894b7bc4720591acce7
di3go0-0 <d.rincon@utp.edu.co>
2025-03-21 22:54:31 -0500

    matrix lib

---
+++ matrix/src/main.rs
@@ -0,0 +1,22 @@
+use class::class::Matrix;
+
+mod class;
+fn main() {
+    let mut matrix = Matrix::new(3, 3);
+    println!("Matrix:\n{}", matrix);
+
+    matrix.set(0, 0, 4.0).unwrap();
+    matrix.set(0, 1, 1.0).unwrap();
+    matrix.set(0, 2, 3.0).unwrap();
+    matrix.set(1, 0, 2.0).unwrap();
+    matrix.set(1, 1, 1.0).unwrap();
+    matrix.set(1, 2, 4.0).unwrap();
+    matrix.set(2, 0, 0.0).unwrap();
+    matrix.set(2, 1, 1.0).unwrap();
+    matrix.set(2, 2, 2.0).unwrap();
+
+    println!("Matrix:\n{}", matrix);
+
+    let data = matrix.get(0, 1);
+    println!("Matrix:\n{:?}", matrix);
+}
